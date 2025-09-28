1d64868791ded39d8b821743367de4bea03e92c7
Wei <huangwei@burncloud.com>
2025-09-29 00:11:54 +0800

    🔧 chore: remove crates dir

--- crates/cli/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod commands;
-pub mod output;
-
-pub use commands::*;
-pub use output::*;
--- crates/core/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod config_manager;
-pub mod model_manager;
-
-pub use config_manager::*;
-pub use model_manager::*;
